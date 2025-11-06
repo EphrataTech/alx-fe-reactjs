cc820a96f5e22a90b7dafa6446e8cc2fb8e8c6ee
Ephrata <ephrata0327@gmail.com>
2025-11-06 16:28:48 +0300

    finish task 03

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,4 +1,9 @@
-function UserDetails({ userData }) {
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
+
+const userData = useContext(UserContext);
+function UserDetails() {
   return (
     <div>
       <p>Name: {userData.name}</p>
