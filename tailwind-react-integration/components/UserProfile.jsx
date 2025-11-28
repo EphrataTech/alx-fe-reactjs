d5d114024b17537857197147936d59102b9636a9
Ephrata <ephrata0327@gmail.com>
2025-11-28 20:29:02 +0300

    add styling

---
+++ tailwind-react-integration/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+function UserProfile() {
+  return (
+    <div className="user-profile">
+        <div className="container" bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg> 
+      <img src="https://via.placeholder.com/150" alt="User" rounded-full w-36 h-36 mx-auto  />
+      <h1 text-xl text-blue-800 my-20>John Doe</h1>
+      <p text-gray-400 text-base>Developer at Example Co. Loves to write code and explore new technologies.</p>
+    </div>
+    </div>
+  );
+}
+
+export default UserProfile;
